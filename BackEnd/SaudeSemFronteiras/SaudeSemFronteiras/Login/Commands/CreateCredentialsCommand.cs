8d6e71c2c8c255f470d62f2abd1b577f1ffce924
Jean Azzolini <jeanazzolini@gmail.com>
2024-07-15 21:54:52 -0300

    Criado handler do objeto Login

--- BackEnd/SaudeSemFronteiras/SaudeSemFronteiras/Login/Commands/CreateCredentialsCommand.cs
+++ BackEnd/SaudeSemFronteiras/SaudeSemFronteiras/Login/Commands/CreateCredentialsCommand.cs
@@ -1,9 +1,19 @@
 ﻿using CSharpFunctionalExtensions;
 using MediatR;
+using System.Xml.Linq;
 
 namespace SaudeSemFronteiras.Application.Login.Commands;
 public class CreateCredentialsCommand : IRequest<Result>
 {
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
+    public Result Validation()
+    {
+        if (string.IsNullOrEmpty(Email))
+            return Result.Failure("Email não pode ser nulo");
+        if (string.IsNullOrEmpty(Password))
+            return Result.Failure("Password não pode ser nulo");
+
+        return Result.Success();
+    }
 }
---
+++ BackEnd/SaudeSemFronteiras/SaudeSemFronteiras/Login/Handlers/CredentialsHandler.cs
@@ -0,0 +1,49 @@
+﻿using CSharpFunctionalExtensions;
+using MediatR;
+using SaudeSemFronteiras.Application.Login.Commands;
+using SaudeSemFronteiras.Application.Login.Domain;
+using SaudeSemFronteiras.Application.Login.Repository;
+
+namespace SaudeSemFronteiras.Application.Login.Handlers;
+public class CredentialsHandler : IRequestHandler<CreateCredentialsCommand, Result>,
+                                  IRequestHandler<ChangeCredentialsCommand, Result>
+{
+    private readonly ICredentialsRepository _credentialsRepository;
+
+    public CredentialsHandler(ICredentialsRepository credentialsRepository)
+    {
+        _credentialsRepository = credentialsRepository;
+    }
+
+    public async Task<Result> Handle(CreateCredentialsCommand request, CancellationToken cancellationToken)
+    {
+        var validationResult = request.Validation();
+
+        if (validationResult.IsFailure)
+            return validationResult;
+
+        var credentials = Credentials.Create(request.Email, request.Password);
+
+        await _credentialsRepository.Insert(credentials, cancellationToken);
+
+        return Result.Success();
+    }
+    public async Task<Result> Handle(ChangeCredentialsCommand request, CancellationToken cancellationToken)
+    {
+        var credentials = await _credentialsRepository.GetById(request.Id, cancellationToken);
+        if (credentials == null)
+            return Result.Failure("Usuário não cadastrado");
+
+        var validationResult = request.Validation();
+
+        if (validationResult.IsFailure)
+            return validationResult;
+
+        credentials.Update(request.Email, request.Password);
+
+        await _credentialsRepository.Update(credentials, cancellationToken);
+
+        return Result.Success();
+    }
+}
+
